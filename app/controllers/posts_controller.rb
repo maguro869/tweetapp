d7f695ea8fef66826f0b94a91cdf510b5eae70ea
maguro869 <maguro869@gmail.com>
2021-05-17 14:17:39 +0900

    [UPDATE]postをuserで識別できるように、user詳細ページで該当ユーザのpostを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
-
+  before_action :set_current_user
   before_action :authenticate_user
+  before_action :ensure_corrent_user, {only: [:edit, :update, :destroy ]}
+
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -8,6 +10,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find_by(id:params[:id])
+    @user = @post.user
   end
 
   def new
@@ -15,7 +18,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
 
     if @post.save
       flash[:notice] = "投稿を作成しました"
@@ -46,4 +52,13 @@ class PostsController < ApplicationController
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
 class Post < ApplicationRecord
-    validates :content, {
+    validates :content, { 
         presence: true, 
         length: { 
             maximum: 140 
             } 
         }
+    validates :user_id, {
+        presence: true
+    }
+
+    def user
+        return User.find_by(id: self.user_id)
+    end
 end
